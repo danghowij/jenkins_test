2dc9f517bdf25a16dc1e024199125d2247515715
Hoi Dang <danghowij@gmail.com>
2023-12-19 23:56:28 +0700

    change code to test webhook again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('Hello world');
+  res.send('Hello world 2 commit');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
